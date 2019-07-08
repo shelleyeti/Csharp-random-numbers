de430dabfeed91a74dd23cfb6936a1713d5fc655
Shelley Arnold <shelle.kay@gmail.com>
2019-07-08 18:45:31 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random_numbers
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Random random = new Random();
+      List<int> numbers = new List<int> {
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    random.Next(6),
+    };
+
+      Console.WriteLine($"Initial List of Random Numbers: ");
+      numbers.ForEach(n => Console.WriteLine(n));
+
+      Console.WriteLine("Conditional Number List Contains: ");
+      for (int i = 0; i < numbers.Count; i++)
+      {
+        string output = numbers.Contains(i) ? $"List does contain {i}" : $"List does not contain {i}";
+        Console.WriteLine(output);
+      }
+
+
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Practice: Random Numbers
+
+## Instructions
+Using the given code, produce the something similar to the following outputs:
+1. numbers list contains 0
+1. numbers list does not contain 1
+1. numbers list does not contain 2
+1. numbers list contains 3
+1. numbers list contains 4
+1. numbers list does not contains 5
+
+### Results will appear in the console
